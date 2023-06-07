ecc0025b7402b22d1402428f9536ae79d69a7f23
SeongSikChae <55735227+SeongSikChae@users.noreply.github.com>
2023-06-07 15:47:29 +0900

    BinaryWriter WriteKleiString Extension

---
+++ OniSaveFileSerializer/System/IO/BinaryWriterExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace System.IO
+{
+    using Text;
+
+    public static class BinaryWriterExtensions
+    {
+        public static void WriteKleiString(this BinaryWriter writer, string? value) 
+        {
+            if (value is null)
+            {
+                writer.Write(-1);
+                return;
+            }
+
+            if (string.Empty.Equals(value))
+            {
+                writer.Write(0);
+                return;
+            }
+
+            byte[] buf = Encoding.UTF8.GetBytes(value);
+            writer.Write(buf.Length);
+            writer.Write(buf);
+        }
+    }
+}
---
+++ OniSaveFileSerializerTest/System/IO/BinaryWriterExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.IO.Tests
+{
+    [TestClass]
+    public class BinaryWriterExtensionsTests
+    {
+        [TestMethod]
+        public void WriteKleiStringTest()
+        {
+            const string expected = "Hello, World";
+
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            writer.WriteKleiString(expected);
+            memory.Position = 0;
+
+            string? actual = reader.ReadKleiString();
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void WriteKleiStringTest2()
+        {
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            writer.WriteKleiString(null);
+            memory.Position = 0;
+
+            string? actual = reader.ReadKleiString();
+            Assert.IsNull(actual);
+        }
+
+        [TestMethod]
+        public void WriteKleiStringTest3()
+        {
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            writer.WriteKleiString(string.Empty);
+            memory.Position = 0;
+
+            string? actual = reader.ReadKleiString();
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(string.Empty, actual);
+        }
+    }
+}
