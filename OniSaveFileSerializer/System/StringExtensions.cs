28f6e9d7b0a131d3c50ad06251792c029b66ccc6
SeongSikChae <55735227+SeongSikChae@users.noreply.github.com>
2023-06-07 14:58:06 +0900

    String ValidateDotNetIdentifierName Extension

---
+++ OniSaveFileSerializer/System/StringExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace System
+{
+    using Text.RegularExpressions;
+
+    public static class StringExtensions
+    {
+        public static void ValidateDotNetIdentifierName(this string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new NullOrEmptyIdentifierNameException("A .NET identifier name must not be null or zero length");
+            if (name.Length >= 512)
+                throw new MaxLengthIdentifierNameException("A .NET identifier name exceeded 511 characters. This most likely indicates a parser error.");
+            if (IDENTIFIER_INVAL_CHARS.IsMatch(name))
+                throw new NonPrintalbeIdentifierNameException("A .NET identifier name contains non-printable characters. This most likely indicates a parser error.");
+        }
+
+        private static readonly Regex IDENTIFIER_INVAL_CHARS = new Regex("[\x00-\x1F]");
+    }
+
+    public sealed class NullOrEmptyIdentifierNameException : Exception
+    {
+        public NullOrEmptyIdentifierNameException(string message, Exception? innerException = null) : base(message, innerException) { }
+    }
+
+    public sealed class MaxLengthIdentifierNameException : Exception
+    {
+        public MaxLengthIdentifierNameException(string message, Exception? innerException = null) : base(message, innerException) { }
+    }
+
+    public sealed class NonPrintalbeIdentifierNameException : Exception
+    {
+        public NonPrintalbeIdentifierNameException(string message, Exception? innerException = null) : base(message, innerException) { }
+    }
+}
---
+++ OniSaveFileSerializerTest/System/StringExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Tests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        public void ValidateDotNetIdentifierNameTest()
+        {
+            string? str = null;
+            Assert.ThrowsException<NullOrEmptyIdentifierNameException>(() =>
+            {
+                str.ValidateDotNetIdentifierName();
+            });
+
+            char[] charBuf = new char[512];
+            for (int i = 0; i < charBuf.Length; i++)
+                charBuf[i] = 'A';
+            str = new string(charBuf);
+            Assert.ThrowsException<MaxLengthIdentifierNameException>(() =>
+            {
+                str.ValidateDotNetIdentifierName();
+            });
+
+            charBuf = new char[10];
+            charBuf[0] = '\x00';
+            for (int i = 1; i < charBuf.Length; i++)
+                charBuf[i] = 'A';
+            str = new string(charBuf);
+            Assert.ThrowsException<NonPrintalbeIdentifierNameException>(() =>
+            {
+                str.ValidateDotNetIdentifierName();
+            });
+        }
+    }
+}
