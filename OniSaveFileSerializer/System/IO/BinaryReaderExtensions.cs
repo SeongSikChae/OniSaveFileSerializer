51678d345c798b8d4a43bfe4bdadea3b79c66875
SeongSikChae <55735227+SeongSikChae@users.noreply.github.com>
2023-06-07 14:56:24 +0900

    BinaryReader ReadKleiString Extension

---
+++ OniSaveFileSerializer/System/IO/BinaryReaderExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace System.IO
+{
+    using Text;
+
+    public static class BinaryReaderExtensions
+    {
+        public static string? ReadKleiString(this BinaryReader reader)
+        {
+            int count = reader.ReadInt32();
+            if (count < -1)
+                throw new IOKleiStringException($"Invalid byte count in ReadKleiString: {count}");
+            if (count == -1)
+                return null;
+            if (count == 0)
+                return string.Empty;
+            byte[] buf = reader.ReadBytes(count);
+            return Encoding.UTF8.GetString(buf);
+        }
+    }
+
+    public sealed class IOKleiStringException : IOException
+    {
+        public IOKleiStringException(string message, Exception? innerException = null) : base(message, innerException) { }
+    }
+}
---
+++ OniSaveFileSerializerTest/System/IO/BinaryReaderExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace System.IO.Tests
+{
+    [TestClass]
+    public class BinaryReaderExtensionsTests
+    {
+        [TestMethod]
+        public void ReadKleiStringTest()
+        {
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            byte[] block = Encoding.UTF8.GetBytes("Hello, World");
+            writer.Write(block.Length);
+            writer.Write(block, 0, block.Length);
+
+            memory.Position = 0;
+
+            string? kleiStr = reader.ReadKleiString();
+            Assert.IsNotNull(kleiStr);
+            Assert.AreEqual("Hello, World", kleiStr);
+        }
+
+        [TestMethod]
+        public void ReadKleiStringTest2()
+        {
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            writer.Write(-2);
+            memory.Position = 0;
+
+            Assert.ThrowsException<IOKleiStringException>(() =>
+            {
+                reader.ReadKleiString();
+            });
+        }
+
+        [TestMethod]
+        public void ReadKleiStringTest3()
+        {
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            writer.Write(-1);
+            memory.Position = 0;
+
+            string? kleiStr = reader.ReadKleiString();
+            Assert.IsNull(kleiStr);
+        }
+
+        [TestMethod]
+        public void ReadKleiStringTest4()
+        {
+            using MemoryStream memory = new MemoryStream();
+            using BinaryReader reader = new BinaryReader(memory);
+            using BinaryWriter writer = new BinaryWriter(memory);
+
+            writer.Write(0);
+            memory.Position = 0;
+
+            string? kleiStr = reader.ReadKleiString();
+            Assert.IsNotNull(kleiStr);
+            Assert.AreEqual(string.Empty, kleiStr);
+        }
+    }
+}
